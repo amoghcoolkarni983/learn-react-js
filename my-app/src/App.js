f7303d03a160e0175ae8abffe7d9234fc2d9bdb5
Amogh Kulkarni <amogh.kulkarni@cerner.com>
2019-10-05 04:57:34 -0600

    adding functional componants

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Greet from './componants/Greet'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        Hello World
-        </header>
+        <Greet/>
       </div>
     );
   }
---
+++ my-app/src/componants/Greet.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Greet = () => <h1>Hello Amogh</h1>
+
+export default Greet
