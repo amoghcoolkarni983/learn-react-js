efca43dac1f0526212d7fe12f1768697e44e780b
Amogh Kulkarni <amogh.kulkarni@cerner.com>
2019-10-05 07:16:46 -0600

    learning state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import Greet from './componants/Greet'
-import Welcome from './componants/Welcome'
+import Message from './componants/Message'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Greet name = "Amogh" avenger = "Ironman" />
-        <Greet name = "Cool" avenger = "Captain America" />
-
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <Welcome name = "Class Componant Name" avenger = "and Its avenger" />
-   
+        <Message/>
       </div>
     );
   }
