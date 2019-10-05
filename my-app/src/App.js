48fe2dd49d654a70c0c5029bbc73e7710a977b07
Amogh Kulkarni <amogh.kulkarni@cerner.com>
2019-10-05 05:24:52 -0600

    class-componant

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Greet from './componants/Greet'
+import Welcome from './componants/Welcome'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Greet/>
+        <Welcome/>
       </div>
     );
   }
